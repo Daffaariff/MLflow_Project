5c07ee7c3cf03d1f259a60840bf278175bee5d26
Bistowsky <dafa.130.df@gmail.com>
2024-03-31 20:16:57 +0700

    model evaluation added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -3,7 +3,8 @@ from mlProject.utils.common import read_yaml, create_directories
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            ModelTrainerConfig)
+                                            ModelTrainerConfig,
+                                            ModelEvaluationConfig)
 
 
 class ConfigurationManager:
@@ -88,3 +89,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+
+
+    # model evaluation
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/Daffaariff/MLflow_Project.mlflow",
+    )
+
+        return model_evaluation_config
